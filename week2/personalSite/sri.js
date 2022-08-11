50622f313643cfd2a370ed4a2db24c4e8ef57dbe
Hisham Paloli <hishampaloli786@gmail.com>
2022-08-11 16:21:04 +0530

    linkedInClone

--- week2/personalSite/sri.js
+++ week2/personalSite/sri.js
@@ -21,7 +21,7 @@ loading.style.display = 'none';
 form.addEventListener('submit',(e) => {
     if(namee.value === ''){
         nameL.innerText = "Name cannot be empty"
-    }else if(!isNaN(namee.value) ){
+    }else if(/\d/.test(namee.value) ){
         nameL.innerText =  "Name should be alphabets"
     }else {
         nameL.innerText = ''
@@ -42,7 +42,7 @@ form.addEventListener('submit',(e) => {
 
     if(Email.value === ''){
         emailL.innerText = 'This feild cannot be empty';
-    }else if(Email.value.match( /^[^ ]+@[^ ]+\.[a-z]{2,3}$/) ){
+    }else if(Email.value.match(/^[^ ]+@[^ ]+\.[a-z]{2,3}$/) ){
         emailL.innerText = ''
     }else {
         emailL.innerText = 'Should use a valid email.'
