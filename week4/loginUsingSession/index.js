01d6af1b59e902e5b22e35e2ced728c5660217a6
Hisham Paloli <hishampaloli786@gmail.com>
2022-08-25 11:20:54 +0530

    session

--- week4/loginUsingSession/index.js
+++ week4/loginUsingSession/index.js
@@ -1,16 +1,14 @@
-
 const express = require("express");
-const ejsMate = require('ejs-mate');
+const ejsMate = require("ejs-mate");
 const mongoose = require("mongoose");
-const path = require("path");
-var bodyParser = require("body-parser");
-var cookieParser = require("cookie-parser");
-var session = require("express-session");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const bcrypt = require('bcrypt');
 
 const User = require("./user");
 
 const app = express();
-
 
 mongoose.connect("mongodb://localhost:27017/userSession", {
   useNewUrlParser: true,
@@ -25,15 +23,12 @@ db.once("open", () => {
 
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views","views");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-
-
 app.use(cookieParser());
-
 
 app.use(
   session({
@@ -48,79 +43,91 @@ app.use(
 );
 
 app.use((req, res, next) => {
-    if (req.cookies.user_sid && !req.session.user) {
-      res.clearCookie("user_sid");
-    }
+  if (req.cookies.user_sid && !req.session.user) {
+    res.clearCookie("user_sid");
+  }
+  next();
+});
+
+var sessionChecker = (req, res, next) => {
+  if (req.session.user && req.cookies.user_sid) {
+    res.redirect("/dashboard");
+  } else {
     next();
-  });
+  }
+};
 
-  var sessionChecker = (req, res, next) => {
-    if (req.session.user && req.cookies.user_sid) {
-      res.redirect("/dashboard");
-    } else {
-      next();
-    }
-  };
+app.get("/", sessionChecker, (req, res) => {
+  res.redirect("login");
+});
 
-  app.get("/", sessionChecker, (req, res) => {
-    res.redirect('login');
-  })
-
-  app.route("/login")
-  .get( sessionChecker, (req, res) => {
+app
+  .route("/login")
+  .get(sessionChecker, (req, res) => {
     res.render("login");
   })
-  .post(async(req, res) => {
-    const user = await User.findOne({name: req.body.name, password: req.body.password});
+  .post(async (req, res) => {
+    const user = await User.findOne({
+      name: req.body.name
+    });
 
-    if (user) {
-        req.session.user = user
-        res.redirect("/dashboard");
-    }else {
-        res.redirect("/login");
+    const valid = await bcrypt.compare(req.body.password, user.password);
+
+    if (valid) {
+      req.session.user = user;
+      res.redirect("/dashboard");
+    } else {
+      res.redirect("/login");
     }
-  })
+  });
 
-  app.route("/signup")
+app
+  .route("/signup")
   .get(sessionChecker, (req, res) => {
-    res.render('signup');
+    res.render("signup");
   })
-  .post( async(req, res) => {
-    const user = new User(req.body.bod);
+  .post(async (req, res) => {
+    const hash = await bcrypt.hash(req.body.bod.password, 10);
 
-    const userExist = await User.findOne({$or: [{name : req.body.bod.name}, {email : req.body.bod.email}]});
+    const user = new User({
+      name: req.body.bod.name,
+      email: req.body.bod.email,
+      password: hash,
+    });
+
+    const userExist = await User.findOne({
+      $or: [{ name: req.body.bod.name }, { email: req.body.bod.email }],
+    });
 
     if (!userExist) {
-        await user.save((err, doc) => {
-            if (err) {
-                res.redirect('/signup')
-            }else {
-                console.log(doc);
-                req.session.user = doc;
-                res.redirect("/dashboard")
-            }
-        });
-    }else {
-        res.redirect("/signup");
+      await user.save((err, doc) => {
+        if (err) {
+          res.redirect("/signup");
+        } else {
+          console.log(doc);
+          req.session.user = doc;
+          res.redirect("/dashboard");
+        }
+      });
+    } else {
+      res.redirect("/signup");
     }
-    
-    
-  })
+  });
 
-  app.get("/dashboard", (req, res) => {
-    // console.log(req.session.user?.name,"**************");
-    if (!req.session.user) {
-        res.redirect("/login");  
-    }else {
-        res.render("dashboard",{name: req.session.user.name});
-    }
-  })
-
-  app.get("/logout", (req, res) => {
-    req.session.user = undefined;
+app.get("/dashboard", (req, res) => {
+  // console.log(req.session.user?.name,"**************");
+  if (!req.session.user) {
     res.redirect("/login");
-  })
+  } else {
+    res.render("dashboard", { name: req.session.user.name });
+  }
+});
 
-  app.listen(3000, () => {
-    console.log("786 ready to go");
-  })
+app.get("/logout", (req, res) => {
+  req.session.user = undefined;
+  res.redirect("/login");
+});
+
+app.listen(3000, () => {
+  console.log("786 ready to go");
+});
